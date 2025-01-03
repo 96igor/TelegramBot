42299a9a71907989d5bfcb4adf97d2ce5cdb533f
Игорь Царёв <tsarov88@gmail.com>
2025-01-03 09:38:19 +0200

    Initial commit

---
+++ src/main/java/job/Project29/Project29Application.java
@@ -0,0 +1,13 @@
+package job.Project29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project29Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project29Application.class, args);
+	}
+
+}
